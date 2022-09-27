dbe3825ba5d0149f569a8476aaa45680e6eb6ab1
Brady Byrd <brabyrd@gmail.com>
2022-09-26 18:04:59 -0700

    day-2

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ blah blah
 */
 console.log("Brady Byrd");
 console.log("my favorite food is chicken");
-var longWord = "Supercalifragilisticexpialidocious";
-var myData = longWord.length;
+let longWord = "Supercalifragilisticexpialidocious";
+let myData = longWord.length;
 console.log(myData);
 console.log(typeof 22);
 console.log(typeof 17n);
